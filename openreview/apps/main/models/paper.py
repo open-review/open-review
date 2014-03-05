ff09113f1e303fbbea60e2fde0b748eecd9475c8
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-03-05 11:09:26 +0100

    Deleted URL models, replaced it with a simpel TextField

--- openreview/apps/main/models/paper.py
+++ openreview/apps/main/models/paper.py
@@ -14,26 +14,18 @@ class Keyword(models.Model):
         app_label = "main"
 
 
-class URL(models.Model):
-    url = models.TextField()
-    title = models.TextField(null=True)
-
-    class Meta:
-        app_label = "main"
-
-
 class Paper(models.Model):
     doc_id = models.TextField(help_text="Identifier: can either be a real DOI or a domain-specific one. "
                                         "For example: arXiv:1403.0438.", null=True)
     title = models.TextField()
     abstract = models.TextField()
-    publisher = models.TextField(null=True)
+    publisher = models.TextField(null=True, blank=True)
     publish_date = models.DateField(null=True)
+    urls = models.TextField(null=True, blank=True)
 
     # These fields are *probably* a bad idea performance wise.
     authors = models.ManyToManyField(Author)
     keywords = models.ManyToManyField(Keyword)
-    urls = models.ManyToManyField(URL)
 
     def get_reviews(self):
         return self.reviews.filter(parent__isnull=True)
