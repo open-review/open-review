b0757af6fca25f162908f252ebf1159ccbf01e08
Thomas Brus <thomas.brus@me.com>
2014-03-06 11:44:00 +0100

    Restored papers urls

--- openreview/urls.py
+++ openreview/urls.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'openreview.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include("openreview.apps.accounts.urls")),
-    # url(r'^papers/', include("openreview.apps.papers.urls")),
+    url(r'^papers/', include("openreview.apps.papers.urls")),
     url(r'^', include("openreview.apps.main.urls"))
 )
