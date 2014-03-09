05b976ed7066c89bf8a5db0797a9224a99b1b5ea
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-03-09 11:23:51 +0100

    Added name parameter to PaperWithReviewsView to allow easy url resolving

--- openreview/apps/papers/urls.py
+++ openreview/apps/papers/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from openreview.apps.papers.views import PaperWithReviewsView
 
 urlpatterns = patterns('',
-    url(r'^(?P<id>\d+)/$', PaperWithReviewsView.as_view())
+    url(r'^(?P<id>\d+)/$', PaperWithReviewsView.as_view(), name="paper")
 )
