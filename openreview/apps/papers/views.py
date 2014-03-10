55697683dd2592257b4afd837979d87b2cc10ef9
Thomas Brus <thomas.brus@me.com>
2014-03-10 20:10:22 +0100

    Fetch paper and reviews from db

--- openreview/apps/papers/views.py
+++ openreview/apps/papers/views.py
@@ -1,4 +1,12 @@
 from django.views.generic import TemplateView
+from openreview.apps.main.models import Paper
+from django.shortcuts import render
 
 class PaperWithReviewsView(TemplateView):
     template_name = "papers/paper-with-reviews.html"
+
+
+    def get(self, request, id):
+      return render(request, "papers/paper-with-reviews.html", {
+          "paper": Paper.objects.get(pk=id)
+      })
