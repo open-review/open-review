6a0955bdad6cd19a267097a63852ce29de50d10a
Wietze <wietzuh@gmail.com>
2014-03-27 14:14:12 +0100

    Fixed page number handling, included some tests

---
+++ openreview/apps/papers/tests/papers.py
@@ -0,0 +1,25 @@
+import unittest
+
+from django.core.urlresolvers import reverse
+from django.test import Client
+
+from openreview.apps.tools.testing import create_test_paper
+
+__all__ = ["TestPapersView"]
+
+class TestPapersView(unittest.TestCase):
+
+    def test_get(self): 
+        create_test_paper()   
+        c = Client()
+        subpages = subpages = ["new","trending","controversial"];
+        for subpage in subpages:
+            url = reverse(subpage)
+            self.assertEqual(c.get(url).status_code, 200)
+            self.assertEqual(c.get(url + "?p=0").status_code, 200)
+            self.assertEqual(c.get(url + "?p=1").status_code, 200)
+            self.assertEqual(c.get(url + "?p=999").status_code, 200)
+            self.assertEqual(c.get(url + "?p=-1").status_code, 404)
+            self.assertEqual(c.get(url + "?p=").status_code, 404)
+            self.assertEqual(c.get(url + "?p=abc").status_code, 404)
+            self.assertEqual(c.get(url + "?p=1abc").status_code, 404)
