d07e39e194f052ea04eb6df656b064a46033d927
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-04-05 00:11:40 +0200

    Added related_name=children to Category.parent

--- openreview/apps/main/models/category.py
+++ openreview/apps/main/models/category.py
@@ -1,13 +1,11 @@
 from django.db import models
-from django.conf import settings
 
 __all__ = ["Category"]
-
 
 class Category(models.Model):
     name = models.TextField()
     arxiv_code = models.TextField(null=True, unique=True)
-    parent = models.ForeignKey("main.Category", null=True)
+    parent = models.ForeignKey("main.Category", null=True, related_name="children")
 
     def __str__(self):
         return self.name
