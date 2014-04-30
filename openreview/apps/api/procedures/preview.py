3b5109d6ce9901e71e29501f45cbd4b3682c1f9a
Martijn Bastiaan <martijn.bastiaan@gmail.com>
2014-04-30 22:37:45 +0200

    Added procedures API, added PreviewProcedure

---
+++ openreview/apps/api/procedures/preview.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.forms import ModelChoiceField
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from openreview.apps.main.forms import ReviewForm
+from openreview.apps.main.models import Paper
+
+class PreviewForm(ReviewForm):
+    paper = ModelChoiceField(queryset=Paper.objects.all())
+
+    def save(self, commit=True):
+        review = super().save(commit=False)
+        review.paper = self.cleaned_data["paper"]
+        return review
+
+class PreviewProcedure(APIView):
+    """Renders review as it would anywhere else on the website. This procedure
+    always returns text/html. You can use the visibility parameter to render
+    anonymous, public or external reviews. Your options are `public`,
+    `semi_anonymous`, `anonymous`, and `external`. You need also provide a
+    `paper` parameter."""
+    def get(self, request, format=None):
+        return Response({"details": "POST only."})
+
+    @method_decorator(login_required)
+    def post(self, request, format=None):
+        preview_form = PreviewForm(user=request.user, data=request.DATA)
+        if not preview_form.is_valid():
+            return Response({"details": preview_form._errors}, status=400)
+
+        # We need to set internal cache to prevent queries to an object which
+        # does not exist in the database.
+        review = preview_form.save(commit=False)
+        review.id = 0
+        review._n_comments = 0
+        review._n_downvotes = 0
+        review._n_upvotes = 0
+        review._reviews_children = {0: []}
+        review.timestamp = datetime.now()
+
+        return render(request, "papers/review.html", {"review": review})
+
--- openreview/apps/api/urls.py
+++ openreview/apps/api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 from rest_framework import routers
+from openreview.apps.api import procedures
+from openreview.apps.api.procedures import ProcedureViewSet
 
-from openreview.apps.api.viewsets import user, paper, author, category, review, keyword
+from openreview.apps.api.viewsets import user, paper, author, category, keyword
 from openreview.apps.api.viewsets.keyword import KeywordViewSet
 
 top_router = routers.DefaultRouter()
@@ -11,7 +13,7 @@ top_router.register(r'papers', paper.PaperViewSet)
 top_router.register(r'authors', author.AuthorViewSet)
 top_router.register(r'categories', category.CategoryViewSet)
 top_router.register(r'keywords', KeywordViewSet)
-top_router.register(r'reviews', review.ReviewViewSet)
+top_router.register(r'procedures', ProcedureViewSet, base_name="procedure")
 
 users_router = routers.DefaultRouter()
 users_router.register(r'reviews', user.ReviewViewSet)
@@ -28,12 +30,15 @@ keywords_router.register(r'papers', keyword.PaperViewSet)
 categories_router = routers.DefaultRouter()
 categories_router.register(r'papers', category.PaperViewSet)
 
+procedures_router = routers.DefaultRouter()
+
 urlpatterns = patterns('',
     url(r'^', include(top_router.urls)),
     url(r'^users/(?P<user_id>[0-9]+)/', include(users_router.urls)),
     url(r'^papers/(?P<paper_id>[0-9]+)/', include(papers_router.urls)),
     url(r'^keywords/(?P<keyword_id>[0-9]+)/', include(keywords_router.urls)),
     url(r'^categories/(?P<category_id>[0-9]+)/', include(categories_router.urls)),
+    url(r'^procedures/', include(procedures.urlpatterns)),
 )
 
 # Set API root documentation
