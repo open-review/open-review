eed6812314127b0a3df2f920c4cbaf7190cbddd5
Wietze <wietzuh@gmail.com>
2014-05-17 16:37:13 +0200

    Created mission and team pages

--- openreview/apps/main/urls.py
+++ openreview/apps/main/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import patterns, url
 
 from openreview.apps.main.views import landing_page
 from openreview.apps.main.views import dashboard
+from django.views.generic.base import TemplateView
 
 urlpatterns = patterns('',
     url('^$', landing_page, name="landing_page"),
-    url('^dashboard$', dashboard, name="dashboard")
+    url('^dashboard$', dashboard, name="dashboard"),
+    url(r'^about/team', TemplateView.as_view(template_name='main/team.html'), name='team'),
+    url(r'^about/mission$', TemplateView.as_view(template_name='main/mission.html'), name="mission")
 )
