3b8945b8d9739b3cb99c2a03c069878a2d93106a
Thomas Brus <thomas.brus@me.com>
2014-06-13 18:45:26 +0200

    Removed duplicate tests, again

--- openreview/apps/papers/tests/paper.py
+++ openreview/apps/papers/tests/paper.py
@@ -6,27 +6,13 @@ from openreview.apps.tools.testing import BaseTestCase
 from openreview.apps.tools.testing import create_test_paper, assert_max_queries, create_test_user, SeleniumTestCase
 
 
-__all__ = ["TestPaperWithReviewsView", "TestPaperViewLive"]
+__all__ = ["TestPaperViewLive"]
 
-
-class TestPaperWithReviewsView(BaseTestCase):
+class TestPaperViewLive(SeleniumTestCase):
     def setUp(self):
         self.paper = create_test_paper(2, 2, 2, 2)
         self.client = Client()
 
-    def test_queries_anonymous(self):
-        with assert_max_queries(n=11):
-            self.client.get(reverse("paper", args=[self.paper.id]))
-
-    def test_queries_logged_in(self):
-        create_test_user(username="abc", password="123")
-        self.assertTrue(self.client.login(username="abc", password="123"))
-
-        with assert_max_queries(n=14):
-            self.client.get(reverse("paper", args=[self.paper.id]))
-
-
-class TestPaperViewLive(SeleniumTestCase):
     def test_new_review(self):
         """
         Merely clicking on 'new review' textarea should span a preview
